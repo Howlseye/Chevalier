dd9eb51d1615837ba8762aff95594463028c60f0
Howlseye <nikolasagala@gmail.com>
2024-12-14 21:25:32 +0700

    + kondisi menang

--- Tugas1/src/Main.kt
+++ Tugas1/src/Main.kt
@@ -2,84 +2,85 @@ import kotlin.random.Random
 
 fun main() {
     println("Minesweeper !!!")
-    println("1. Mulai")
-    println("2. Keluar")
-    println("Pilih: ")
-
-    var pilih = pilih()
-    when (pilih) {
-        1 -> {
-            var mine1 = buatMine(1)
-            var mine2 = buatMine(2)
-            var mineKosong = cekKosong(mine1)
-            var gameOver = false
-            var status = "Mulai"
-            while (!gameOver) {
-                if(mineKosong) {
-                    println("------------------- $status -------------------")
-                    printMine(mine1)
-                    println("Pilih Koordinat")
-                    var x: Int = koordinat("x")
-                    var y: Int = koordinat("y")
-                    println("Mengecek koordinat ($x,$y)")
-
-                    while (mine1[x - 1][y - 1] == '1') {
-                        println("koordinat ($x,$y) sudah terbuka, pilih koordinat lain!")
-                        x = koordinat("x")
-                        y = koordinat("y")
-                    }
-
-                    var cek = cekMine(mine2, x, y)
-                    if (cek) {
-                        status = "Bukan Bom"
-                        mine1[x - 1][y - 1] = '1'
-                        println("------------------- $status -------------------")
-                        printMine(mine1)
-
-                    }else {
-                        status = "Duarr"
-                        mine1[x - 1][y - 1] = '*'
-                        println("------------------- $status -------------------")
-                        printMine(mine1)
-                        gameOver = true
-                        println("Yowai Mo")
-                        println("Terrorist Win")
-
-                    }
-                }else {
-                    println("Counter-Terrorist Win")
-                    break
-                }
+    var pilih = pilih("Mulai", "Keluar", 1)
+    while ((pilih[0] == 1 && (pilih[1] == 1) || pilih[1] == 2)) {
+        val mine1 = buatMine(1)
+        val mine2 = buatMine(2)
+        printMine(mine2)
+        var mineDibuka = 0
+        var gameOver = false
+        var status = "Mulai"
+        while (!gameOver && mineDibuka <= 19) {
+            println("------------------- $status -------------------")
+            printMine(mine1)
+            println("Pilih Koordinat")
+            var x: Int = koordinat("x")
+            var y: Int = koordinat("y")
+            while (mine1[ubahIndex(x)][ubahIndex(y)] == '1') {
+                println("Koordinat ($x,$y) sudah terbuka, pilih koordinat lain!")
+                x = koordinat("x")
+                y = koordinat("y")
             }
-            println("1. Ulangi")
-            println("1. Ulangi")
+            println("Mengecek koordinat ($x,$y)")
 
+            val cek = cekMine(mine2, x, y)
+            if (cek) {
+                status = "Bukan Bom"
+                mine1[ubahIndex(x)][ubahIndex(y)] = '1'
+                mineDibuka++
+            }else {
+                status = "Duarr"
+                mine1[ubahIndex(x)][ubahIndex(y)] = '*'
+                println("------------------- $status -------------------")
+                printMine(mine1)
+                gameOver = true
+                println("Yowai Mo")
+                println("Gitu doang kalah")
+            }
+        }
+        if (!gameOver && mineDibuka == 20 ) {
+            status = "Menang"
+            println("------------------- $status -------------------")
+            printMine(mine1)
+            println("Hoki doang itu")
+        }
+        pilih = pilih("Main lagi", "Dah ah cape", 2)
+        if(pilih[0] == 2 && pilih[1] == 2) {
+            printMine(mine2)
+            pilih = pilih("Main lagi", "Keluar", 1)
 
         }
-        2 -> println("Keluar....")
     }
-
+    if (pilih[1] == 2)
+        println("Gitu doang nyerah T_T")
+    println("Keluar....")
 }
 
-fun pilih() :Int {
-    println("Minesweeper !!!")
-    println("1. Mulai")
-    println("2. Keluar")
+fun pilih(s: String, t: String, u: Int) :List<Int> {
+    val v = u + 1
+    println("1. $s")
+    if(u == 2)
+        println("2. Lihat posisi bom")
+    println("$v. $t")
     println("Pilih: ")
 
     var pilih: Int = -1
     while (pilih == -1) {
-        val input = readlnOrNull()?.toInt()
-        if (input == 1 || input == 2)
-            pilih = input
+        val input= readLine()?.toIntOrNull()
+        if (input != null) {
+            if (((input == 1 || input == 2) && u == 1) || ((input in 1..3) && u == 2))
+                pilih = input
+            else
+                println("Pilihan apaan itu!")
+        }
         else
-            println("Pilihan Tidak Valid!")
+            println("Pilihan apaan itu!")
     }
-    return pilih
+    return listOf(pilih, u)
 }
 
 fun buatMine(mine: Int) :Array<Array<Char>> {
-    var mineHasil = Array(5) { Array(5) { ' ' } }
+    val mineHasil = Array(5) { Array(5) { ' ' } }
     if (mine == 1) {
         for (i in 0..4) {
             for (j in 0..4) {
@@ -94,8 +95,8 @@ fun buatMine(mine: Int) :Array<Array<Char>> {
         }
         var bom = 0
         while (bom < 5) {
-            var randomBaris = Random.nextInt(4)
-            var randomKolom = Random.nextInt(4)
+            val randomBaris = Random.nextInt(4)
+            val randomKolom = Random.nextInt(4)
             if (mineHasil[randomBaris][randomKolom] != '*') {
                 mineHasil[randomBaris][randomKolom] = '*'
                 bom++
@@ -109,7 +110,7 @@ fun printMine(mine: Array<Array<Char>>) {
     println("# Y 1 2 3 4 5")
     println("X |==========")
     for (i in 0..4) {
-        var baris = i + 1
+        val baris = i + 1
         print("$baris |")
         for (j in 0..4) {
             print(" " + mine[i][j])
@@ -122,9 +123,9 @@ fun koordinat(z:String) :Int {
     var n: Int = -1
     while (n == -1) {
         print("$z : ")
-        var input = readLine()?.toInt()
+        val input = readLine()?.toIntOrNull()
         if (input != null) {
-            if (input > -1 && input < 6)
+            if (input in 1..5)
                 n = input
             else
                 println("Invalid")
@@ -134,20 +135,13 @@ fun koordinat(z:String) :Int {
     return n
 }
 
-fun cekKosong(mine: Array<Array<Char>>) :Boolean {
-    var status = false
-    for (baris in mine) {
-        for (kolom in baris) {
-            if (kolom == '_')
-                status = true
-        }
-    }
-    return status
+fun ubahIndex(n:Int):Int {
+    return n - 1
 }
 
 fun cekMine(mine: Array<Array<Char>>, x: Int , y: Int ) :Boolean {
-    if (mine[x - 1][y - 1] == '*')
-        return false
+    return if (mine[ubahIndex(x)][ubahIndex(y)] == '*')
+        false
     else
-        return true
+        true
 }
